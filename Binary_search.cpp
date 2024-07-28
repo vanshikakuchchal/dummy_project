2551046c64ff758f83a8f3a8de1049a082b4c4a2
vanshikakuchchal <vkuchchal@gmail.com>
2024-07-28 14:15:25 +0530

    Binary search code

---
+++ Binary_search.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,key;
+    cin>>n;
+    int *arr;
+    arr= new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"Enter the key";
+    cin>>key;
+    int s=0,e=n-1;
+    while(s<=e)
+    {
+        int mid = (s+e)/2;
+        if(arr[mid] == key)
+        {
+            cout<<"Element found";
+            break;
+        }
+        else if(arr[mid] < key)
+        {
+            s=mid+1;
+
+        }
+        else
+        {
+            e=mid-1;
+               }
+        
+    }
+    cout<<"Element not found";
+}
