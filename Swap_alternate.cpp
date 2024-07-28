82906944eb725ad474c998049cc09c8e7a14a777
vanshikakuchchal <vkuchchal@gmail.com>
2024-07-28 14:29:52 +0530

    commiyttt

---
+++ Swap_alternate.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[200];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    if(n%2 ==0)
+    {
+        for(int i=0;i<n-1;i=i+2)
+        {
+            int temp=arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1]= temp;
+        }
+    }
+    else
+    {
+        for(int i=0;i<n-2;i=i+2)
+        {
+            int temp=arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1]= temp;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
